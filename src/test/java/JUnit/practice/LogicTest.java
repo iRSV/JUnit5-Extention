aa23ab8f70e7aa0790490bb2667262f9a355a4dd
asl2D <asll2D@yandex.ru>
2019-05-21 19:23:58 +0300

    Try JMH.

--- src/test/java/JUnit/practice/LogicTest.java
+++ src/test/java/JUnit/practice/LogicTest.java
@@ -3,6 +3,10 @@ package JUnit.practice;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +16,24 @@ class LogicTest {
 
     private final Logic logic = new Logic();
 
-    @Benchmark
     @Test
+    @Benchmark
     void fillArrayList() {
-        assertEquals(logic.getListLength(),logic.fillArrayList().size());
+        assertEquals(logic.getListLength(), logic.fillArrayList().size());
     }
 
     @Test
-    public void fillLinkedList(){
-        assertEquals(logic.getListLength(),logic.fillLinkedList().size());
+    public void fillLinkedList() {
+        assertEquals(logic.getListLength(), logic.fillLinkedList().size());
+    }
+
+    public static void benchmarkRunner() throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(LogicTest.class.getSimpleName())  // место для перехвата имени нужного класса в расшинерии
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
     }
 }
